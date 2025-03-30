d23df95f87e0814a2781ab3b043c0f8020300c81
liyunjie0208 <12131715@me.mcu.edu.tw>
2025-03-30 14:40:18 +0800

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using MyMvcApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace MyMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+    }
+}
